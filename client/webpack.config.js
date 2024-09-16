1dd9f8b192e1d8dfd1653cb13bb9ba85b265875b
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-09-15 22:26:12 -0400

    Changed project name to CodeNova and Updated PWA config.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,71 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
+    // Set the mode to development
     mode: 'development',
+    // Define entry points for the bundle
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js'
     },
+    // Define output configuration
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // Define plugins
     plugins: [
-      
+      // Generate HTML file and inject bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'CodeNova'
+      }),
+      // Inject service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Generate manifest.json for PWA
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'CodeNova',
+        short_name: 'CodeNova',
+        description: 'A sleek, modern text editor for your coding needs',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
+    // Define module rules for different file types
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
